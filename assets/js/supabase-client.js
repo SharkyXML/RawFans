f22f17338cb18588bc61526e3f036b6099185a58
SharkyXML <lsharky216@gmail.com>
2026-06-12 17:24:16 +0200

    Rawfans

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -6,7 +6,7 @@
  */
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://wyandwrbzrlxjvxvjds.supabase.co';
+const SUPABASE_URL = 'https://wyandwrbzrlxvjvxvjds.supabase.co';
 const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5YW5kd3JienJseHZqdnh2amRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNjg5NzYsImV4cCI6MjA5Njg0NDk3Nn0.2mHu4S4YTT5xS_ZhH5tlE2c5jq8OUaxfweg-rse-Wic';
 
